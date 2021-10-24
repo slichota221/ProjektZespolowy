ed758ef3b91902dfd60385139241091086e75bb9
slichota <s.lichota.221@studms.ug.edu.pl>
2021-10-24 16:45:21 +0200

    first commit

---
+++ src/main/java/pl/ug/Projekt/Zespolowy/domain/Game.java
@@ -0,0 +1,14 @@
+package pl.ug.Projekt.Zespolowy.domain;
+
+import java.util.Date;
+
+public class Game {
+
+    private int ID;
+    private String nameGame;
+    private Date dateRelase;
+    private String description;
+    private int IdCover;
+    private int IdGenre;
+    private int IdPublisher;
+}
---
+++ src/test/java/pl/ug/Projekt/Zespolowy/ProjektZespolowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ug.Projekt.Zespolowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektZespolowyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
