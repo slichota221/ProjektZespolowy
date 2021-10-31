41a2ade954859e33ab2865192f6c11b80f5fa581
slichota <s.lichota.221@studms.ug.edu.pl>
2021-10-31 19:54:35 +0100

    add repositories

---
+++ src/main/java/pl/ug/Projekt/Zespolowy/repository/PublisherRepository.java
@@ -0,0 +1,7 @@
+package pl.ug.Projekt.Zespolowy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.ug.Projekt.Zespolowy.domain.Publisher;
+
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+}
