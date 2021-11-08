74adba3f38817e5b18b08d38ab12fa28f9b81bba
Szymon Porębski <szymonporebski@hotmail.com>
2021-11-08 17:26:19 +0100

    fix - gain access to css files

--- src/main/java/pl/ug/Projekt/Zespolowy/config/WebSecurityConfig.java
+++ src/main/java/pl/ug/Projekt/Zespolowy/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package pl.ug.Projekt.Zespolowy.config;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,6 +32,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(authenticationUserService)
                 .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/styles/**");
     }
 
 }
