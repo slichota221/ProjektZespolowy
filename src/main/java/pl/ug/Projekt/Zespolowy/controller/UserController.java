f2a0bcbac8493a3f9e67f84580cd453cf103cf6d
pawlowski99 <pawlowski3141@gmail.com>
2021-12-13 21:54:51 +0100

    add user profile - from session

--- src/main/java/pl/ug/Projekt/Zespolowy/controller/UserController.java
+++ src/main/java/pl/ug/Projekt/Zespolowy/controller/UserController.java
@@ -1,5 +1,7 @@
 package pl.ug.Projekt.Zespolowy.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -10,6 +12,8 @@ import pl.ug.Projekt.Zespolowy.repository.GenreRepository;
 import pl.ug.Projekt.Zespolowy.repository.PublisherRepository;
 import pl.ug.Projekt.Zespolowy.repository.UserRepository;
 
+import java.security.Principal;
+
 @Controller
 public class UserController {
 
@@ -18,6 +22,13 @@ public class UserController {
     UserController(UserRepository userRepository){
         this.userRepository = userRepository;
 
+    }
+
+    @GetMapping("/web/user")
+    String getUser(Model model, Principal principal) {
+        model.addAttribute("viewedUser", userRepository.findByUsername(principal.getName()));
+
+        return "user-profile";
     }
 
     @GetMapping("/web/users")
